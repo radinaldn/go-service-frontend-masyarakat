8a87116f3f497bce31cf9ec163087921df4aa2d3
Radina Dwiki N <radinal.dwiki.novendra@students.uin-suska.ac.id>
2019-01-07 01:34:04 +0700

    menambahkan push notif

---
+++ app/src/main/java/id/topapp/radinaldn/goservicemasyarakat/model/PemesananUnrated.java
@@ -0,0 +1,8 @@
+package id.topapp.radinaldn.goservicemasyarakat.model;
+
+/**
+ * Created by radinaldn on 06/01/19.
+ */
+
+public class PemesananUnrated {
+}
---
+++ app/src/main/java/id/topapp/radinaldn/goservicemasyarakat/response/ResponseAddPemesananRating.java
@@ -0,0 +1,8 @@
+package id.topapp.radinaldn.goservicemasyarakat.response;
+
+/**
+ * Created by radinaldn on 06/01/19.
+ */
+
+public class ResponseAddPemesananRating {
+}
